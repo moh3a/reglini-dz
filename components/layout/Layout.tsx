5a1115c6571a890062b01c6d5a19b79f10fd3f19
moh3a <aitabdelmalekma@gmail.com>
2021-11-13 21:06:04 +0100

    finished data gathering for postal codes

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,13 +1,11 @@
 import StoreNavigation from "./nav/StoreNavigation";
 import Footer from "./Footer";
 import { ReactNode } from "react";
-import Breadcrumbs from "../Breadcrumb";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <StoreNavigation />
-      <Breadcrumbs />
       <main>{children}</main>
       <Footer />
     </>
